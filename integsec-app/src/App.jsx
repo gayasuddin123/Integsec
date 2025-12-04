e4511390f8c10991218ddc07fe4216bd2e0ae3a9
Gayasuddin <gyasuansari145@gmail.com>
2025-12-04 13:26:11 +0530

    logo change

--- integsec-app/src/App.jsx
+++ integsec-app/src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Homepage from './components/Homepage'
-import './App.css'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <Homepage></Homepage>
-    </>
+    <div>
+      <Homepage/>
+    </div>
   )
 }
 
